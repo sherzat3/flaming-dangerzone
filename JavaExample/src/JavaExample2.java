c57349c4271be65e0dea35b8610a9d08e9f69754
sherzat3 <sherzat3@gmail.com>
2014-09-17 18:47:40 -0500

    Added checking tests

--- JavaExample/src/JavaExample2.java
+++ JavaExample/src/JavaExample2.java
@@ -1,17 +1,39 @@
+package Example2;
 import java.util.Arrays;
 
 
 public class JavaExample2 {
 
+	public static void main(String[] args) {
+		System.out.println("Fibonacci 7th element: " + fibonacci(7));
+		System.out.println("Factorial of 6       : " + factorial(6));
+		System.out.println("All primes until 100 : ");
+		primes(100);
+		System.out.println("Sort given array: 5 4 10 -3 3 2 1 0 20");
+		int a[] = {5, 4, -10, 3, 2, 1, 0, 20};
+		a = sort(a);
+		for(int i = 0; i < 8; ++i)
+			System.out.print(a[i] + " ");
+		System.out.println();
+		
+		System.out.println("Compare strings \"abc\" and \"abc\"");
+		char[] stringA = {'a', 'b', 'c'}, stringB = {'a', 'b', 'c'};
+		System.out.println(compareTwoStrings(stringA, stringB));
+		
+		System.out.println("Compare string \"aaa\" and \"bbb\"");
+		stringA = new char[] {'a', 'a', 'a'};
+		stringB = new char[] {'b', 'b', 'b'};
+		System.out.println(compareTwoStrings(stringA, stringB));
+	}
 
-	public int fibonacci(int q){
+	public static int fibonacci(int q){
 		if(q == 1 || q == 2) return 1;
 		
 		else
 			return fibonacci(q - 2) + fibonacci(q - 1);
 	}
 	
-	public int factorial(int q){
+	public static int factorial(int q){
 		if(q == 1)
 			return 1;
 		
@@ -19,18 +41,18 @@ public class JavaExample2 {
 			return q * factorial(q - 1);
 	}
 	
-	public int[] primes(int q){
+	public static void primes(int q){
 		int[] a = new int[1000];
 		int n = 0;
 		
 		for(int i = 2; i <= q; ++i)
 			if(isPrime(i))
-				a[n++] = i;
+				System.out.print(i + " ");
 		
-		return a;
+		System.out.println();
 	}
 	
-	public boolean isPrime(int q){
+	public static boolean isPrime(int q){
 		if(q == 1) return false;
 		
 		for(int i = 2; i * i <= q; ++i)
@@ -40,7 +62,7 @@ public class JavaExample2 {
 		return true;
 	}
 	
-	public int[] sort(int[] a){
+	public static int[] sort(int[] a){
 		for(int i = 0; i < a.length - 1; ++i){
 			for(int j = i + 1; j < a.length; ++j)
 				if(a[i] > a[j]){
@@ -53,22 +75,13 @@ public class JavaExample2 {
 		return a;
 	}
 	
-	public boolean compareTwoStrings(char[] a, char[] b){
+	public static boolean compareTwoStrings(char[] a, char[] b){
 		
 		if(a.length != b.length) return false;
 		
-		int[] letters = new int[30];
-		Arrays.fill(letters, 0);
-		
-		for(int i = 0; i < a.length; ++i){
-			letters[(int)(a[i] - 'a')]++;
-			letters[(int)(b[i] - 'a')]--;
-		}
-		
-		for(int i = 0; i < 30; ++i){
-			if(letters[i] != 0)
+		for(int i = 0; i < a.length; ++i)
+			if(a[i] != b[i])
 				return false;
-		}
 		
 		return true;
 		
